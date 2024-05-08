8b8d6f46f731969c102207de95445cf5c46ca90e
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-05-08 18:28:06 +0200

    Add project files.

---
+++ Sterk-typing-og-verdityper-Oppgaver/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Sterk_typing_og_verdityper_Oppgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oppgave 1
+            void Oppgave1()
+            {
+                int number = 0;
+                long number2 = 0;
+
+                decimal decimals = 0.24M;
+                float decimals2 = 0.22F;
+                double decimals3 = 0.55D;
+
+                string text = "Dette er en tekst";
+                char character = 'Y';
+
+                bool IsThisTrue = true;
+            }
+            
+            // Oppgave 2
+            int a = 5;
+            decimal b = 3M;
+
+            decimal sum = a + b;
+            // sum må være en decimal fordi jeg tror at:
+            // int kan bli konvertert til en decimal, men en decimal kan ikke bli kovertert til en int uten å bruke Convert.ToInt32()
+            int sum2 = a + Convert.ToInt32(b);
+        }
+    }
+}
